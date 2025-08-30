bda3ca26001a6ec4e75961bbb705d0fbe37d076d
RIOKOWI <achyarrio2004@gmail.com>
2025-08-30 11:08:00 +0700

    ONE TO ONE POLYMORPHIC : edit model customer & product

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\HasOneThrough;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
 use Illuminate\Support\Facades\Date;
 use Symfony\Component\HttpKernel\Debug\VirtualRequestStack;
 
@@ -53,5 +54,11 @@ class Customer extends Model
         return $this->belongsToMany(Product::class, 'customers_likes_products', 'customer_id', 'product_id')->withPivot('created_at')
         ->wherePivot('created_at', '>=', Date::now()->addDays(-7))
         ->using(Like::class);
+    }
+
+    // one to one polymorphic
+    public function image(): MorphOne
+    {
+        return $this->morphOne(Image::class, 'imageable');
     }
 }
